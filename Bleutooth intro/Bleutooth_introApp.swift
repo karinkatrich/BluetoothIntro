3d120a6e9d7d361f1a6bc2a956ac89ffe4d12680
Karyna Katrich <karyna.katrich@mhp.com>
2022-07-11 15:00:29 +0200

    Initial Commit

---
+++ Bleutooth intro/Bleutooth_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bleutooth_introApp.swift
+//  Bleutooth intro
+//
+//  Created by Karyna Katrich on 11.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Bleutooth_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
